ff25b02703db17702bc478cdc084a99a02ecb28d
NataliiaRomanenko <nataliia.romanenko@gmail.com>
2018-08-22 15:02:21 +0300

    creating block with cards

--- app/js/common.js
+++ app/js/common.js
@@ -1,5 +1,15 @@
 $(function() {
 
-	// Custom JS
+    var swiper = new Swiper('.slider', {
+        loop: true,
+        // autoplay: {
+        //     delay: 2500,
+        //     disableOnInteraction: false,
+        // },
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+        },
+    });
 
 });
